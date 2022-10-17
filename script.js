baedaf89236fb9451ad5b9442c0ee4b129635ffd
DelcidesNeto <115945133+DelcidesNeto@users.noreply.github.com>
2022-10-17 00:56:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function entrou(){
+    var link = window.document.querySelector('a')
+    link.style.textDecoration = 'underline'
+}
+
+
+function saiu(){
+    var link = window.document.querySelector('a')
+    link.style.textDecoration = 'none'
+}
+
+
+function voltar(){
+    window.location.href = 'index.html'
+}
